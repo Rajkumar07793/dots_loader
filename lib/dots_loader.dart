e3491f0c907fe5f68c9ebe3c1e3403cc54803191
Raj kumar Patel <Rajkumar07793@gmail.com>
2021-06-01 18:15:11 +0530

    Initial commit

---
+++ lib/dots_loader.dart
@@ -0,0 +1,74 @@
+library dots_loader;
+
+import 'package:flutter/material.dart';
+
+class ShowDots extends StatefulWidget {
+  final List<int> duration;
+  final int noOfDots;
+  final List<Color> colors;
+  ShowDots({Key key, this.noOfDots, this.colors, this.duration});
+  @override
+  _ShowDotsState createState() => _ShowDotsState();
+}
+
+class _ShowDotsState extends State<ShowDots> {
+  List<int> duration = [400, 410, 420];
+  @override
+  Widget build(BuildContext context) {
+    List<int> duration = widget.duration??[400, 410, 420];
+    int noOfDots = widget.noOfDots??3;
+    List<Color> colors = widget.colors??[Colors.red, Colors.green, Colors.yellowAccent];
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: List<Widget>.generate(
+            noOfDots,
+                (index) => LoadingDots(color: colors[index],duration: duration[index],)
+        ),
+      ),
+    );
+  }
+}
+
+
+class LoadingDots extends StatefulWidget {
+  final duration;
+  final color;
+  LoadingDots({Key key, this.duration, this.color}):super(key: key);
+  @override
+  _LoadingDotsState createState() => _LoadingDotsState();
+}
+
+class _LoadingDotsState extends State<LoadingDots>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animate();
+  }
+
+  animate() {
+    animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: widget.duration));
+    animation = Tween<double>(begin: 0, end: 20).animate(animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+    animationController.repeat(reverse: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: animation.value,
+      width: animation.value,
+      decoration: BoxDecoration(
+        color: widget.color,
+        borderRadius: BorderRadius.circular(100),
+      ),
+    );
+  }
+}
